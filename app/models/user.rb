76deb5a1ea82cef27259134e618d5077a7e0cdc0
Juan C. Müller <jcmuller@gmail.com>
2014-02-23 11:56:02 -0500

    Remove rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ActiveRecord::Base
-  rolify
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
     belongs_to :team
-    
+
 	devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
 
